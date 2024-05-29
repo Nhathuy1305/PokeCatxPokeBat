0f38fadafebf368401b2df3aa420506ece13ba0c
Nhathuy1305 <ITITIU20043@student.hcmiu.edu.vn>
2024-05-29 21:42:10 +0700

    update crawler

--- pkg/crawler/types.go
+++
@@ -1,9 +0,0 @@
-package crawler
-
-type Pokemon struct {
-	ID                int                `json:"id"`
-	Name              string             `json:"name"`
-	Types             []string           `json:"types"`
-	Stats             map[string]int     `json:"stats"`
-	DamageMultipliers map[string]float64 `json:"damage_multipliers"`
-}
